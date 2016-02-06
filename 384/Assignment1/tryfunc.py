fabbe14f46aaa0d84ca207b74bb248d059d375e2
andrewshao123 <andrewshao123@hotmail.com>
2016-02-06 12:31:38 -0500

    updated

---
+++ 384/Assignment1/tryfunc.py
@@ -0,0 +1,7 @@
+class Vehicle(object):
+	def __init__(self, name, loc, length, is_horizontal, is_goal):
+		self.name = name
+		self.length = length
+		self.loc = loc
+		self.is_horizontal = is_horizontal
+		self.is_goal = is_goal
